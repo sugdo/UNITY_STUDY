508db327290226ff1f1e53e49c4415c1294a68e3
Gwang Cheon <thousand419@google.com>
2018-11-15 23:13:55 +0900

    rulette

---
+++ Roulette/Assets/Scenes/rouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rouletteController : MonoBehaviour {
+    float rotSpeed = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            this.rotSpeed = 22;
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.96f;
+	}
+}

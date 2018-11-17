b14e98babd7fe0e2ef6fba7dc3fd34a49f51e700
Gwang Cheon <thousand419@google.com>
2018-11-17 21:27:14 +0900

    car game

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speed = 0;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos=Input.mousePosition;
+        }
+        else if ( Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+            speed = swipeLength / 500f;
+            GetComponent<AudioSource>().Play();
+        }
+        
+
+        transform.Translate(speed, 0, 0);
+        this.speed *= 0.98f;
+
+       
+	}
+}
